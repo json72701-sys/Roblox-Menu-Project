f1f3e07c46a6af4392f29714a2ef73f501a519bf
json72701-sys <json72701@gmail.com>
2026-04-24 21:37:46 -0500

    Update main.mm

--- main.mm
+++ main.mm
@@ -1,44 +1,19 @@
-#include "imgui.h"
-#include <string>
+#include <UIKit/UIKit.h>
 
-// Variables to track the menu state
-bool show_menu = false;
-static char script_buffer[99999] = ""; // This holds your pasted script
-
-void RenderUI() {
-    // 1. THE FLOATING BUTTON [G]
-    ImGui::SetNextWindowPos(ImVec2(50, 50), ImGuiCond_FirstUseEver);
-    ImGui::Begin("Toggle", nullptr, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoBackground);
-    if (ImGui::Button("[G]", ImVec2(50, 50))) {
-        show_menu = !show_menu; // Clicking the G opens/closes the menu
-    }
-    ImGui::End();
-
-    // 2. THE MAIN EXECUTOR MENU
-    if (show_menu) {
-        ImGui::SetNextWindowSize(ImVec2(400, 300), ImGuiCond_FirstUseEver);
-        ImGui::Begin("Gold Executor v1.0", &show_menu);
-
-        ImGui::Text("Paste your script below:");
+__attribute__((constructor))
+static void initialize() {
+    // This waits for the game to actually exist before popping the UI
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(15 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        UIWindow *win = [[UIApplication sharedApplication] keyWindow];
         
-        // The Script Input Box
-        ImGui::InputTextMultiline("##ScriptBox", script_buffer, IM_ARRAYSIZE(script_buffer), ImVec2(-FLT_MIN, 180));
-
-        ImGui::Separator();
-
-        // The Execute Button
-        if (ImGui::Button("EXECUTE", ImVec2(120, 40))) {
-            // This is where the Lua injection happens
-            // For now, it will just clear the box to show it "sent"
-            printf("Executing: %s\n", script_buffer);
-        }
-
-        ImGui::SameLine();
-
-        if (ImGui::Button("Clear", ImVec2(80, 40))) {
-            memset(script_buffer, 0, sizeof(script_buffer));
-        }
-
-        ImGui::End();
-    }
+        // Create a Gold Button to open the menu
+        UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+        [btn setFrame:CGRectMake(20, 40, 60, 60)];
+        [btn setTitle:@"GOLD" forState:UIControlStateNormal];
+        [btn setBackgroundColor:[UIColor orangeColor]];
+        btn.layer.cornerRadius = 30;
+        btn.layer.zPosition = 10000;
+        
+        [win addSubview:btn];
+    });
 }
