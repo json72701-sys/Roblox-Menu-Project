1e01a27fded19b74316cf028322d10d91d12d94b
json72701-sys <json72701@gmail.com>
2026-04-24 22:07:55 -0500

    Update main.mm

--- main.mm
+++ main.mm
@@ -1,6 +1,6 @@
 #import <UIKit/UIKit.h>
 
-// This tells the compiler that the function exists in your C++ files
+// This "extern" fixes the "Undefined Symbol" error by linking to your C++ code
 extern "C" void RenderImGuiMenu(bool visible);
 
 static bool isMenuVisible = false;
@@ -12,30 +12,28 @@ static bool isMenuVisible = false;
     CGPoint lastPoint;
 }
 
-// Logic to make the button follow your finger
+// Logic to make the logo draggable
 - (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     UITouch *touch = [touches anyObject];
     CGPoint currentLocation = [touch locationInView:self.superview];
     self.center = currentLocation;
 }
 
-// Logic to toggle the menu when tapped
+// Logic to toggle your ImGui menu when tapped
 - (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     UITouch *touch = [touches anyObject];
     if (touch.tapCount == 1) {
         isMenuVisible = !isMenuVisible;
-        RenderImGuiMenu(isMenuVisible); // Calls your ImGui toggle
+        RenderImGuiMenu(isMenuVisible);
     }
 }
 @end
 
 __attribute__((constructor))
 static void initialize() {
-    // Wait for the game window to load
+    // Wait for game window to load (reduced to 10 seconds for speed)
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(10 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         UIWindow *win = nil;
-        
-        // Find the active window in the application
         for (UIWindow *window in [UIApplication sharedApplication].windows) {
             if (window.isKeyWindow) {
                 win = window;
@@ -47,10 +45,8 @@ static void initialize() {
             DraggableLogo *btn = [DraggableLogo buttonWithType:UIButtonTypeCustom];
             btn.frame = CGRectMake(100, 100, 60, 60);
             
-            // Modern Orange/Gold look
             [btn setBackgroundColor:[UIColor orangeColor]];
             [btn setTitle:@"GOLD" forState:UIControlStateNormal];
-            btn.titleLabel.font = [UIFont boldSystemFontOfSize:12];
             btn.layer.cornerRadius = 30;
             btn.layer.borderWidth = 2;
             btn.layer.borderColor = [UIColor whiteColor].CGColor;
