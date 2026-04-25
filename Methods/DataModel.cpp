2c1ddd8a057862f76b0ae2236b299439822356f9
json72701-sys <json72701@gmail.com>
2026-04-25 12:47:40 -0500

    Update DataModel.cpp

--- Methods/DataModel.cpp
+++ Methods/DataModel.cpp
@@ -1,1 +1,11 @@
+#include "../offsets.hpp"
 
+// This function follows the pointers to find the game's core
+uintptr_t GetDataModel() {
+    // mem::BaseAddress is the start of the Roblox app
+    uintptr_t VisualEngine = *(uintptr_t*)(0x100000000 + offsets::VisualEnginePointer); 
+    uintptr_t DataModelPointer = *(uintptr_t*)(VisualEngine + offsets::VisualEngineToDataModel1);
+    uintptr_t DataModel = *(uintptr_t*)(DataModelPointer + offsets::VisualEngineToDataModel2);
+    
+    return DataModel;
+}
