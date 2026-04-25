43bdeda68e2834566a04421986e445b6988e07ff
json72701-sys <json72701@gmail.com>
2026-04-24 21:01:22 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,31 +1,8 @@
-#include <UIKit/UIKit.h>
-#include <objc/runtime.h>
+#include <stdio.h>
 
-// This is the "Gold Hook" 
-// It waits for the game to load, then creates a window on top of EVERYTHING
+// This is the absolute simplest way to see if your file works
 __attribute__((constructor))
 static void initialize() {
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        
-        // 1. Get the main screen of the iPad
-        UIWindow *window = [[UIApplication sharedApplication] keyWindow];
-        
-        // 2. Create a "Floating Logo" button
-        UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
-        [button setFrame:CGRectMake(100, 100, 60, 60)];
-        [button setTitle:@"GOLD" forState:UIControlStateNormal];
-        [button setBackgroundColor:[UIColor colorWithRed:1.0 green:0.84 blue:0.0 alpha:1.0]]; // Gold Color
-        button.layer.cornerRadius = 30; // Makes it a circle
-        
-        // 3. Add a shadow so you can see it
-        button.layer.shadowColor = [[UIColor blackColor] CGColor];
-        button.layer.shadowOffset = CGSizeMake(0, 2);
-        button.layer.shadowRadius = 5;
-        button.layer.shadowOpacity = 0.5;
-
-        // 4. Stick it to the screen
-        [window addSubview:button];
-        
-        printf("GOLD MENU INJECTED SUCCESSFULLY! ✅\n");
-    });
+    // This doesn't draw a menu, but it proves the file is "alive"
+    printf("--- EXECUTOR INITIALIZED ---");
 }
