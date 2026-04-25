95780323f71e8a97d19a5266df12cf8e51a7a8a6
json72701-sys <json72701@gmail.com>
2026-04-25 13:50:26 -0500

    Update main.mm

--- main.mm
+++ main.mm
@@ -1,21 +1,17 @@
 #import <UIKit/UIKit.h>
 
 // --- CONNECTING YOUR UD LOGIC ---
-// These lines tell the compiler to look inside your folders
 #include "offsets.hpp"
-#include "Methods/DataModel.cpp"
+#include "Methods/DataModel.cpp" 
 // --------------------------------
 
 static bool isMenuVisible = false;
 
-// This is the "Fix" for the Red X error.
-// It creates the function the compiler was looking for.
+// Defining this here so it's only in ONE place
 extern "C" void RenderImGuiMenu(bool visible) {
     if (visible) {
-        // This is your new EXECUTE functionality.
-        // It runs your UD logic the moment you tap the logo!
         uintptr_t dm = GetDataModel(); 
-        NSLog(@"[ElxrScriptz] DataModel found at: %p", (void*)dm);
+        NSLog(@"[ElxrScriptz] DataModel found: %p", (void*)dm);
     }
 }
 
@@ -26,21 +22,20 @@ extern "C" void RenderImGuiMenu(bool visible) {
 - (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     UITouch *touch = [touches anyObject];
     CGPoint currentLocation = [touch locationInView:self.superview];
-    self.center = currentLocation; // Keeps your dragging logic exactly the same
+    self.center = currentLocation;
 }
 
 - (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     UITouch *touch = [touches anyObject];
     if (touch.tapCount == 1) {
         isMenuVisible = !isMenuVisible;
-        RenderImGuiMenu(isMenuVisible); // Toggles the menu and runs the UD logic
+        RenderImGuiMenu(isMenuVisible); 
     }
 }
 @end
 
 __attribute__((constructor))
 static void initialize() {
-    // Waits 10 seconds after the game starts to show your logo
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(10 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         UIWindow *win = nil;
         for (UIWindow *window in [UIApplication sharedApplication].windows) {
@@ -51,7 +46,7 @@ static void initialize() {
             DraggableLogo *btn = [DraggableLogo buttonWithType:UIButtonTypeCustom];
             [btn setFrame:CGRectMake(100, 100, 60, 60)];
             
-            // ELXR Title with the Blue background
+            // Your Blue ELXR Logo
             [btn setTitle:@"ELXR" forState:UIControlStateNormal];
             [btn setBackgroundColor:[UIColor blueColor]]; 
             
