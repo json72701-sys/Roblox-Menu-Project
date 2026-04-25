326a866bd446f68a51c7ac3987bf6b78022285e3
json72701-sys <json72701@gmail.com>
2026-04-24 22:11:40 -0500

    Update main.mm

--- main.mm
+++ main.mm
@@ -1,26 +1,23 @@
 #import <UIKit/UIKit.h>
 
-// This links your Objective-C button to your C++ ImGui menu
+// This links your button to your ImGui menu code
 extern "C" void RenderImGuiMenu(bool visible);
 
 static bool isMenuVisible = false;
 
-// This class handles the "Draggable" logic you requested
+// Custom class to handle dragging
 @interface DraggableLogo : UIButton
 @end
 
-@implementation DraggableLogo {
-    CGPoint lastPoint;
-}
-
-// Moves the logo when you drag your finger
+@implementation DraggableLogo
+// Updates position when you drag
 - (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     UITouch *touch = [touches anyObject];
     CGPoint currentLocation = [touch locationInView:self.superview];
     self.center = currentLocation;
 }
 
-// Opens the menu when you tap the logo
+// Toggles the menu when you tap
 - (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     UITouch *touch = [touches anyObject];
     if (touch.tapCount == 1) {
@@ -32,30 +29,19 @@ static bool isMenuVisible = false;
 
 __attribute__((constructor))
 static void initialize() {
-    // Waits 10 seconds for the game to load
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(10 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         UIWindow *win = nil;
-        
-        // Find the active window to place the button on
         for (UIWindow *window in [UIApplication sharedApplication].windows) {
-            if (window.isKeyWindow) {
-                win = window;
-                break;
-            }
+            if (window.isKeyWindow) { win = window; break; }
         }
 
         if (win) {
             DraggableLogo *btn = [DraggableLogo buttonWithType:UIButtonTypeCustom];
-            btn.frame = CGRectMake(100, 100, 60, 60);
-            
-            // Setting the "Gold" theme
-            [btn setBackgroundColor:[UIColor orangeColor]];
+            [btn setFrame:CGRectMake(100, 100, 60, 60)];
             [btn setTitle:@"GOLD" forState:UIControlStateNormal];
+            [btn setBackgroundColor:[UIColor orangeColor]];
             btn.layer.cornerRadius = 30;
-            btn.layer.borderWidth = 2;
-            btn.layer.borderColor = [UIColor whiteColor].CGColor;
             btn.layer.zPosition = 10000;
-            
             [win addSubview:btn];
         }
     });
