9675b5dc4050b1a4de6e964f911b5e70c0e418d1
json72701-sys <json72701@gmail.com>
2026-04-25 12:54:31 -0500

    Update Player.hpp

--- Structures/Player.hpp
+++ Structures/Player.hpp
@@ -1,1 +1,14 @@
+#ifndef PLAYER_H
+#define PLAYER_H
 
+#include "../offsets.hpp"
+
+// This tells the engine what a 'Player' looks like in memory
+namespace Player {
+    inline uintptr_t GetLocalPlayer(uintptr_t playersService) {
+        // 0x130 is the offset for LocalPlayer from your list!
+        return *(uintptr_t*)(playersService + 0x130); 
+    }
+}
+
+#endif
