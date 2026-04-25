622f1781e435d5bb0065dd0e7f111484e1d5c8b4
json72701-sys <json72701@gmail.com>
2026-04-24 20:36:34 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,23 @@
-#include <UIKit/UIKit.h>
+#include "imgui.h"
+#include <stdio.h>
 
-// This is the "Automatic Trigger"
+// This is a "Constructor" - it runs the SECOND the dylib is loaded
 __attribute__((constructor))
 static void initialize() {
-    // We wait 5 seconds to make sure Roblox is fully open
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        
-        // This creates a standard iPad Alert box
-        UIViewController *root = [UIApplication sharedApplication].keyWindow.rootViewController;
-        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Gold Executor" 
-                                    message:@"Menu Loaded Successfully! ✅" 
-                                    preferredStyle:UIAlertControllerStyleAlert];
-        
-        [alert addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
-        [root presentViewController:alert animated:YES completion:nil];
-    });
+    // This sends a message to the iPad's system log so we know it worked
+    printf("!!! GOLD EXECUTOR LOADED !!!\n");
+}
+
+// This is a simple flag to show the menu
+bool visible = true;
+
+void Render() {
+    if (visible) {
+        ImGui::Begin("Gold Executor", &visible);
+        ImGui::Text("Menu is Running!");
+        if (ImGui::Button("Close")) {
+            visible = false;
+        }
+        ImGui::End();
+    }
 }
